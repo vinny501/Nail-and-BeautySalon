05e66935fbf2d04eb573976750d0b9385b5510f9
Confidence200303 <222927402@mycput.ac.za>
2026-07-19 10:39:45 +0200

    Implemented ProductController and ProductControllerTest

---
+++ src/test/java/za/ac/cput/nailbeautysalon/controller/ProductControllerTest.java
@@ -0,0 +1,37 @@
+package za.ac.cput.nailbeautysalon.controller;
+/*
+ * ProductControllerTest.java
+ * ProductControllerTest class
+ * Author: Confidence Khoza (222927402)
+ * Date: 19 July 2026
+ */
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class ProductControllerTest {
+
+    @Test
+    void a_create() {
+    }
+
+    @Test
+    void b_read() {
+    }
+
+    @Test
+    void c_update() {
+    }
+
+    @Test
+    void d_delete() {
+    }
+
+    @Test
+    void e_getAll() {
+    }
+}
+
+
